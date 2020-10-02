51b6d6c297ee9f2e807e421f4b7edfd6b1dcb451
Derek Holtzman <derekholtzman@gmail.com>
2020-10-01 17:33:54 -0700

    Preparing for socket between client/server for auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { LoremIpsum } from "lorem-ipsum";
 import Header from './components/Header'
 import Article from './components/Article'
+import Authentication from './components/Authentication'
+import feathers from '@feathersjs/client'
+import io from 'socket.io-client';
 
 const lorem = new LoremIpsum({
   sentencesPerParagraph: {
@@ -14,19 +17,51 @@ const lorem = new LoremIpsum({
   }
 });
 
+// Establish a Socket.io connection
+const socket = io('http://localhost:4000');
+
+// Initialize our Feathers client application through Socket.io
+// with hooks and authentication.
+const client = feathers();
+client.configure(feathers.socketio(socket));
+
+// Use localStorage to store our login token
+client.configure(feathers.authentication({
+  storage: window.localStorage
+}));
+
+const login = () => {
+  try {
+    //Attempting to use existing authentication
+    return client.reAuthenticate();
+  } catch (error) {
+    console.log("User is not authenticated :", error);
+  }
+};+
+
+const main = () => {
+  const auth = login();
+  if(auth){
+  // const users = await getAllUsers(auth);
+  console.log(auth)
+  console.log('User is authenticated', client.authentication);
+  }
+};
+
 function App() {
   return (
     <div className="Site">
+      {main()}
+      {/* {console.log(auth)} */}
       <Header />
-      {
+      <Authentication />
       <Article 
-        key={i}
+        key="1"
         title="Title"
         byline="Derek Holtzman"
         publishDate="9/30/2020"
         articleBody={lorem.generateParagraphs(4)}
       />
-      }
     </div>
   );
 }
---
+++ client/src/components/Authentication.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Authentication() {
+    return (
+        <div>
+            <a href='http://localhost:4000/oauth/google'>Login with Google</a>
+            <a href=''>Logout</a>
+        </div>
+    )
+}
+
+export default Authentication
